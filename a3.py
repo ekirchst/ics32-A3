2c4f0dfb10b73aab3c48fa49109b5f2f9650fdec
ekirchst <ekirchst@uci.edu>
2024-02-25 16:50:38 -0800

    brought over past work

---
+++ a3.py
@@ -0,0 +1,17 @@
+# Evan
+# ekirchst@uci.edu
+# 59946460
+
+from pathlib import Path
+import ui as ui
+import admin as admin
+import user as user
+
+
+
+if __name__ == "__main__":
+    if ui.user() == 1:
+        admin.start()
+    else:
+        user.comm_list()
+        user.start()
---
+++ admin.py
@@ -0,0 +1,14 @@
+# Evan
+# ekirchst@uci.edu
+# 59946460
+
+
+from pathlib import Path
+import ui as ui
+import a3 as a3
+
+def start():
+    print("ADMINISTRATOR MODE ENABLED")
+    ui.administration(1)
+    ui.comm()
+    
