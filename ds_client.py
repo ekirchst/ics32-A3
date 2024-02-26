6f6cc2558401b32cc96c5d10c8458f0aa0513a9d
ekirchst <ekirchst@uci.edu>
2024-02-25 22:59:37 -0800

    its not working but... commit anyway

--- ds_client.py
+++ ds_client.py
@@ -3,9 +3,10 @@
 # 59946460
 import socket
 import json
-server_adress = "168.235.86.101"
-server_port =  "3021"
+import ds_protocol
 
+server_adress = "168.235.86.101"
+server_port =  3021
 
 
 def send(server:str, port:int, username:str, password:str, message:str, bio:str=None):
@@ -24,25 +25,33 @@ def send(server:str, port:int, username:str, password:str, message:str, bio:str=
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_conn:
       server_conn.connect((server, port))
       stuff = {}
-      if bio:
-          stuff["token"] = "user_token"
-          stuff["bio"] = {
-            "entry": bio,
-            "timestamp": ""
-            }
-      else:
-          stuff["join"] = {
+      stuff["join"] = {
                     "username": username,
                     "password": password,
                     "token": ""
                 }
-          if message:
-                    stuff["token"] = "user_token"
-                    stuff["post"] = {
-                        "entry": message,
-                        "timestamp": ""
-                    }
       data_str = json.dumps(stuff)
+      server_conn.sendall(data_str.encode())
+      response = server_conn.recv(4096).decode()
+      response_json = json.loads(response)
+      print(response_json)
+      if "token" in str(response_json):
+          temp = str(response_json).index("token")
+          token = str(response_json)[temp+9:-3]
+      if message:
+          action = "post"
+          formatted_message = ds_protocol.format_for_json(action, username, password,user_token=token, message=message, bio=bio)
+          print(formatted_message)
+      elif bio:
+          action = "bio"
+          formatted_message = ds_protocol.format_for_json(action, username, password,user_token=token, message=message, bio=bio)
+          print(formatted_message)
+      else:
+          action = "join"
+          formatted_message = ds_protocol.format_for_json(action, username, password,user_token=token, message=message, bio=bio)
+          print(formatted_message)
+      formatted_message = ds_protocol.format_for_json(action, username, password,user_token=token, message=message, bio=bio)
+      data_str = json.dumps(formatted_message)
       server_conn.sendall(data_str.encode())
 
       response = server_conn.recv(4096).decode()
